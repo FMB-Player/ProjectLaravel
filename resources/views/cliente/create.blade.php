15e00e5db0cfdcfdc6d492776f1d0263fdd53030
FMB_Player <franbalella@hotmail.com.ar>
2024-11-18 22:06:54 -0300

    Prueba Frontend

--- resources/views/cliente/create.blade.php
+++ resources/views/cliente/create.blade.php
@@ -1,3 +1,32 @@
+@component('index')
+    Nuevo cliente
+@endcomponent
+<style>
+    form {
+        padding: 1em;
+        width: 25%;
+        border: 1px solid gray;
+        display: grid;
+        border-radius: 5px;
+    }
+    button {
+        width: min-content;
+    }
+    input {
+        width: 90%;
+        border-radius: 5px;
+        border: 1px solid gray;
+    }
+</style>
 <div>
-    <!-- Knowing is not enough; we must apply. Being willing is not enough; we must do. - Leonardo da Vinci -->
+
+    <form action="{{ route('Clientes.store') }}" method="POST">
+        @csrf
+        <legend>Crear Cliente</legend>
+        <label for="nombre_cliente">Nombre del cliente</label>
+        <input type="text" name="nombre_cliente" id="nombre_cliente" required> <br>
+        <label for="telefono_cliente">Telefono del cliente</label>
+        <input type="number" name="telefono_cliente" id="telefono_cliente" required> <br>
+        <button type="submit">Crear</button>
+    </form>
 </div>
