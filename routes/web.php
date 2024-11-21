e7e7e8b87efdfbc31c6acef33ef270fa84d30743
FMB_Player <franbalella@hotmail.com.ar>
2024-11-21 17:05:19 -0300

    Backend

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\Categoria_ingresoController;
 
+// Asignar pantalla de inicio
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('/Clientes', ClienteController::class);
+Route::resource('/Categorias', Categoria_ingresoController::class);
